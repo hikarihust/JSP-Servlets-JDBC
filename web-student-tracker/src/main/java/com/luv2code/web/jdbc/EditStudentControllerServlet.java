99051782e3a5d4d8275ed97050c293e4a3043db5
vudinhquang <vudinhquangk53@gmail.com>
2022-04-03 16:32:58 +0900

    Update Student - Prepopulating the HTML Form

---
+++ web-student-tracker/src/main/java/com/luv2code/web/jdbc/EditStudentControllerServlet.java
@@ -0,0 +1,72 @@
+package com.luv2code.web.jdbc;
+
+import java.io.IOException;
+
+import javax.annotation.Resource;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+
+/**
+ * Servlet implementation class EditStudentControllerServlet
+ */
+@WebServlet("/edit")
+public class EditStudentControllerServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	private StudentDbUtil studentDbUtil;
+	
+	@Resource(name="jdbc/web_student_tracker")
+	private DataSource dataSource;
+	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		
+		// create our student db util ... and pass in the conn pool / datasource
+		try {
+			studentDbUtil = new StudentDbUtil(dataSource);
+		}
+		catch (Exception exc) {
+			throw new ServletException(exc);
+		}
+	}
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// read student id from form data
+		String theStudentId = request.getParameter("studentId");
+		
+		// get student from database (db util)
+		Student theStudent = null;
+		try {
+			theStudent = studentDbUtil.getStudent(theStudentId);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		// place student in the request attribute
+		request.setAttribute("THE_STUDENT", theStudent);
+		
+		// send to jsp page: update-student-form.jsp
+		RequestDispatcher dispatcher = 
+				request.getRequestDispatcher("/update-student-form.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
